e9fcfe2d99880c7c2d07e388b2736fd4510a96fb
fxanhkhoa <fxanhkhoa@gmail.com>
2018-04-14 11:13:36 +0700

    add gpio

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
  
 int main( int argc, char** argv ) {
   
-  cv::Mat image;
-  image = cv::imread("sample.jpeg" , CV_LOAD_IMAGE_COLOR);
-  
-  if(! image.data ) {
-      std::cout <<  "Could not open or find the image" << std::endl ;
-      return -1;
-    }
-  
-  cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );
-  cv::imshow( "Display window", image );
-  
-  cv::waitKey(0);
-  return 0;
+  	VideoCapture cap;
+	int deviceID = 0;
+	int apiID = cv::CAP_ANY;
+cap.open(deviceID + 0);
+	//cap.open(deviceID + apiID);
+	if (!cap.isOpened())
+{
+		cout << "can't open"<<endl;	
+		return -1;
+}
+	// Device device;
+	while (1)
+	{
+		Mat frame;
+		cap >> frame;
+		imshow("frame", frame);
+		if (waitKey(30) >= 0) break;
+	}
+	return 0;
 }
--- makefile
+++ makefile
@@ -3,10 +3,17 @@ CFLAGS=-g -Wall
 OPENCV = `pkg-config opencv --cflags --libs`
 LIBS=$(OPENCV)
 
-mainmake: ItcarVanishingpoint.cpp TK1_DRIVER/Controller.cpp TK1_DRIVER/JHPWMPCA9685.cpp
-	$(CC) ItcarVanishingpoint.cpp TK1_DRIVER/Controller.cpp TK1_DRIVER/JHPWMPCA9685.cpp -o output $(LIBS)
+mainmake: itcar_contours.cpp TK1_DRIVER/Controller.cpp TK1_DRIVER/JHPWMPCA9685.cpp
+	$(CC) itcar_contours.cpp TK1_DRIVER/Controller.cpp TK1_DRIVER/JHPWMPCA9685.cpp -o output $(LIBS)
 	@echo 'done'
 
+
+testmotor: test_motor.cpp TK1_DRIVER/Controller.cpp TK1_DRIVER/JHPWMPCA9685.cpp TK1_DRIVER/jetsonGPIO.c
+	$(CC) test_motor.cpp TK1_DRIVER/Controller.cpp TK1_DRIVER/JHPWMPCA9685.cpp TK1_DRIVER/jetsonGPIO.c -I TK1_DRIVER -o testmotor
+	@echo 'done'
+
+testcam: main.cpp
+	$(CC) main.cpp -o testcam $(LIBS)
 
 clean:
 	rm -rf *o output
