c8662ff26203cc611c05917aa88308e29d598924
fxanhkhoa <fxanhkhoa@gmail.com>
2018-04-17 12:45:22 +0700

    beautify code

--- main_autocar.cpp
+++ main_autocar.cpp
@@ -4,11 +4,11 @@
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <termios.h>
 #include <string.h>
-#include <sys/time.h>
 #include <string>
+#include <sys/time.h>
+#include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "TK1_DRIVER/Controller.h"
@@ -17,7 +17,7 @@
 using namespace std;
 using namespace cv;
 
-#define MODE_CONTOUR  1
+#define MODE_CONTOUR 1
 #define DEBUG 0
 
 /******** Prototype ***********/
@@ -42,119 +42,117 @@ Controller *_Controller; // Motro and Servo controll variable
 int motor_left, motor_right;
 /*------ End Variables -------*/
 
-int main(int argc, char **argv)
-{
-	jetsonGPIONumber BTN1 = gpio160;
-	jetsonGPIONumber BTN2 = gpio161;
-	jetsonGPIONumber BTN3 = gpio163;
-	jetsonGPIONumber BTN4 = gpio164;
-	gpioExport(BTN1);
- 	gpioExport(BTN2);
-	gpioExport(BTN3);
-	gpioExport(BTN4);
-  gpioSetDirection(BTN1,inputPin);
-	gpioSetDirection(BTN2,inputPin);
-	gpioSetDirection(BTN3,inputPin);
-	gpioSetDirection(BTN4,inputPin);
+int main(int argc, char **argv) {
+  jetsonGPIONumber BTN1 = gpio160;
+  jetsonGPIONumber BTN2 = gpio161;
+  jetsonGPIONumber BTN3 = gpio163;
+  jetsonGPIONumber BTN4 = gpio164;
+  gpioExport(BTN1);
+  gpioExport(BTN2);
+  gpioExport(BTN3);
+  gpioExport(BTN4);
+  gpioSetDirection(BTN1, inputPin);
+  gpioSetDirection(BTN2, inputPin);
+  gpioSetDirection(BTN3, inputPin);
+  gpioSetDirection(BTN4, inputPin);
 
- 	VideoCapture cap(0);
-	_Controller = new Controller();
+  VideoCapture cap(0);
+  _Controller = new Controller();
 
-	// Wait for the push button to be pressed
-   	cout << "Please press the button! ESC key quits the program" << endl;
+  // Wait for the push button to be pressed
+  cout << "Please press the button! ESC key quits the program" << endl;
 
-	unsigned int value = high;
+  unsigned int value = high;
 
-    if (!cap.isOpened())
-      return -1;
+  if (!cap.isOpened())
+    return -1;
   //_Controller->Handle(-45);
-	while(1) {
-		gpioGetValue(BTN1, &value);
-		if (value == low){
-			//_Controller->Speed(100,100);
-			//_Controller->Handle(90);
-			mode = MODE_CONTOUR;
-		}
-		gpioGetValue(BTN2, &value);
-		if (value == low){
-			mode = 0;
-      _Controller->Speed(0,0);
-			_Controller->Handle(0);
-		}
-    if (mode == MODE_CONTOUR){
+  while (1) {
+    gpioGetValue(BTN1, &value);
+    if (value == low) {
+      //_Controller->Speed(100,100);
+      //_Controller->Handle(90);
+      mode = MODE_CONTOUR;
+    }
+    gpioGetValue(BTN2, &value);
+    if (value == low) {
+      mode = 0;
+      _Controller->Speed(0, 0);
+      _Controller->Handle(0);
+    }
+    if (mode == MODE_CONTOUR) {
       cap >> frame;
       motor_left = 100;
       motor_right = 100;
       mode_contour();
     }
-  
 
     if (waitKey(30) == 27)
       break;
-	}
-	
-	cout << "End" << endl;
-	gpioUnexport(BTN1);
+  }
+
+  cout << "End" << endl;
+  gpioUnexport(BTN1);
   gpioUnexport(BTN2);
-	gpioUnexport(BTN3);
-	gpioUnexport(BTN4);
+  gpioUnexport(BTN3);
+  gpioUnexport(BTN4);
   _Controller->Handle(0);
-	_Controller->Speed(0,0);
+  _Controller->Speed(0, 0);
 }
 
-int mode_contour()
-{
-    try {
-      resize(frame, frame, Size(800, 600));
-      
-      // Set region
+int mode_contour() {
+  try {
+    resize(frame, frame, Size(800, 600));
 
-      Rect roi;
-      roi.x = 100;
-      roi.y = 400;
-      roi.width = 600;
-      roi.height = 200;
+    // Set region
 
-      Point carPosition(300,100);
-      Point prvPosition = carPosition;
-      // Crop with ROI
-      crop_img = frame(roi);
-      cvtColor(crop_img, gray, COLOR_BGR2GRAY);
+    Rect roi;
+    roi.x = 100;
+    roi.y = 400;
+    roi.width = 600;
+    roi.height = 200;
 
-      GaussianBlur(gray, gray, Size(15, 15), 1.5, 1.5);
+    Point carPosition(300, 100);
+    Point prvPosition = carPosition;
+    // Crop with ROI
+    crop_img = frame(roi);
+    cvtColor(crop_img, gray, COLOR_BGR2GRAY);
 
-      // bitwise_not(gray, gray);
+    GaussianBlur(gray, gray, Size(15, 15), 1.5, 1.5);
 
-      threshold(gray, thresh, 150, 255, THRESH_BINARY_INV);
+    // bitwise_not(gray, gray);
 
-      //Canny(thresh, edges, 0, 30, 3);
+    threshold(gray, thresh, 150, 255, THRESH_BINARY_INV);
 
-      GetContours();
-      
-      // circle(crop_img, Point(x0,y0), 2, Scalar(5,255,100), CV_FILLED, 8,
-      // 0);
-      //circle(crop_img, center, 3, Scalar(168, 1, 170), CV_FILLED, 8, 0);
-      //imshow("frame1", frame);
-      imshow("vanishing", crop_img);
-      imshow("frame", thresh);
+    // Canny(thresh, edges, 0, 30, 3);
 
-      double theta = getTheta(carPosition, Point(centerx, 50));
-      //cout<< theta << endl;
-      if (abs(theta) > 5){
-        _Controller->Handle(int(theta));
-        
-        if (theta > 0) motor_right -= theta;
-        else if (theta < 0) motor_left -= theta;
+    GetContours();
 
-        _Controller->Speed(motor_left, motor_right);
-      }
-      else{
-        _Controller->Handle(0);
-        _Controller->Speed(100,100);
-      }
+    // circle(crop_img, Point(x0,y0), 2, Scalar(5,255,100), CV_FILLED, 8,
+    // 0);
+    // circle(crop_img, center, 3, Scalar(168, 1, 170), CV_FILLED, 8, 0);
+    // imshow("frame1", frame);
+    imshow("vanishing", crop_img);
+    imshow("frame", thresh);
 
-    } catch (const std::exception &e) {
+    double theta = getTheta(carPosition, Point(centerx, 50));
+    // cout<< theta << endl;
+    if (abs(theta) > 5) {
+      _Controller->Handle(int(theta));
+
+      if (theta > 0)
+        motor_right -= theta;
+      else if (theta < 0)
+        motor_left -= theta;
+
+      _Controller->Speed(motor_left, motor_right);
+    } else {
+      _Controller->Handle(0);
+      _Controller->Speed(100, 100);
     }
+
+  } catch (const std::exception &e) {
+  }
   return 0;
 }
 
@@ -163,53 +161,54 @@ void GetContours() {
   findContours(thresh, contours, hierarchy, CV_RETR_TREE,
                CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-  #if DEBUG
-    cout << thresh.rows << " " << thresh.cols << endl;
-  #endif
+#if DEBUG
+  cout << thresh.rows << " " << thresh.cols << endl;
+#endif
 
-  //Get Moment
-  #if DEBUG
-    long max = 0;
-  #endif
+// Get Moment
+#if DEBUG
+  long max = 0;
+#endif
   int dem = 0;
   centerx = 0; // Reset centerx
   Scalar color =
-          Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+      Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
   if (contours.size() > 0) {
-    vector<Moments> mu(contours.size() );
+    vector<Moments> mu(contours.size());
     Mat drawing = Mat::zeros(thresh.size(), CV_8UC3);
     for (int i = 0; i < contours.size(); i++) {
       double area = contourArea(contours[i]);
       double arclength = arcLength(contours[i], true);
-      if (area > 15000){
+      if (area > 15000) {
         dem++; //(Add number of contour)
-      // Add moment point
-      mu[i] = moments( contours[i], false );
+        // Add moment point
+        mu[i] = moments(contours[i], false);
 
-      #if DEBUG
-        cout << i <<":" << area << " length:"<< arclength << " ";
-        if (max < area) max= area;
-      #endif
-      // MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
-      
-      //Get Mass Center
-      vector<Point2f> mc( contours.size() );
-      mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
-      centerx += mc[i].x;
-      circle( drawing, mc[i], 4, color, -1, 8, 0 );
+#if DEBUG
+        cout << i << ":" << area << " length:" << arclength << " ";
+        if (max < area)
+          max = area;
+#endif
+        // MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
 
-      // double perimeter = Imgproc.arcLength(contour2f, true);
-      // Found squareness equation on wiki...
-      // https://en.wikipedia.org/wiki/Shape_factor_(image_analysis_and_microscopy)
-      // double squareness = 4 * Math.PI * area / Math.pow(perimeter, 2);
+        // Get Mass Center
+        vector<Point2f> mc(contours.size());
+        mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
+        centerx += mc[i].x;
+        circle(drawing, mc[i], 4, color, -1, 8, 0);
 
-      // if (squareness )
-      // if ( hierarchy[i][4] == -1 ) {
-      
-      // Check each point
-      check_left = false;
-      check_right = false;
-      /*for (int j = 0; j < contours[i].size(); j++) {
+        // double perimeter = Imgproc.arcLength(contour2f, true);
+        // Found squareness equation on wiki...
+        // https://en.wikipedia.org/wiki/Shape_factor_(image_analysis_and_microscopy)
+        // double squareness = 4 * Math.PI * area / Math.pow(perimeter, 2);
+
+        // if (squareness )
+        // if ( hierarchy[i][4] == -1 ) {
+
+        // Check each point
+        check_left = false;
+        check_right = false;
+        /*for (int j = 0; j < contours[i].size(); j++) {
         // cout<< i << ": "<< contours[i][j].x <<endl;
         // circle(drawing,contours[i][j],1,cv::Scalar(0,0,255));
         if ((contours[i][j].x < 296))
@@ -220,69 +219,59 @@ void GetContours() {
       #if DEBUG
         cout << i <<" "<< dem <<endl;
       #endif
-      *///if (area > 30000)
+      */ // if (area > 30000)
         drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
-      //}
+        //}
       }
     }
     centerx = centerx / dem;
-    circle( drawing, Point(centerx, 50), 4, color, -1, 8, 0 );
-    //cout<<" max" << max <<endl;
-    #if DEBUG
-      cout << endl;
-    #endif
+    circle(drawing, Point(centerx, 50), 4, color, -1, 8, 0);
+// cout<<" max" << max <<endl;
+#if DEBUG
+    cout << endl;
+#endif
     imshow("Contours", drawing);
   }
 }
 
-
-
-
-
-
-
-
 /*****************************************************************/
 
-
-
-
-
-
 int getkey() {
-    int character;
-    struct termios orig_term_attr;
-    struct termios new_term_attr;
+  int character;
+  struct termios orig_term_attr;
+  struct termios new_term_attr;
 
-    /* set the terminal to raw mode */
-    tcgetattr(fileno(stdin), &orig_term_attr);
-    memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
-    new_term_attr.c_lflag &= ~(ECHO|ICANON);
-    new_term_attr.c_cc[VTIME] = 0;
-    new_term_attr.c_cc[VMIN] = 0;
-    tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
+  /* set the terminal to raw mode */
+  tcgetattr(fileno(stdin), &orig_term_attr);
+  memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
+  new_term_attr.c_lflag &= ~(ECHO | ICANON);
+  new_term_attr.c_cc[VTIME] = 0;
+  new_term_attr.c_cc[VMIN] = 0;
+  tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
 
-    /* read a character from the stdin stream without blocking */
-    /*   returns EOF (-1) if no character is available */
-    character = fgetc(stdin);
+  /* read a character from the stdin stream without blocking */
+  /*   returns EOF (-1) if no character is available */
+  character = fgetc(stdin);
 
-    /* restore the original terminal attributes */
-    tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
+  /* restore the original terminal attributes */
+  tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
 
-    return character;
+  return character;
 }
 
 double getTheta(Point car, Point dst) {
-    if (dst.x == car.x) return 0;
-    if (dst.y == car.y) return (dst.x < car.x ? -90 : 90);
-    //cout << "center x"<< dst.x << " ";
-    double pi = acos(-1.0);
-    double dx = dst.x - car.x;
-    double dy = car.y - dst.y; // image coordinates system: car.y > dst.y
-    #if DEBUG
-      cout <<dx <<" " << dy<<endl;
-    #endif
-    if (dx < 0) return -atan(-dx / dy) * 180 / pi;
-    return atan(dx / dy) * 180 / pi;
+  if (dst.x == car.x)
+    return 0;
+  if (dst.y == car.y)
+    return (dst.x < car.x ? -90 : 90);
+  // cout << "center x"<< dst.x << " ";
+  double pi = acos(-1.0);
+  double dx = dst.x - car.x;
+  double dy = car.y - dst.y; // image coordinates system: car.y > dst.y
+#if DEBUG
+  cout << dx << " " << dy << endl;
+#endif
+  if (dx < 0)
+    return -atan(-dx / dy) * 180 / pi;
+  return atan(dx / dy) * 180 / pi;
 }
-
