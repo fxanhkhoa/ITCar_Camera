7f04599f05f390bed86f51b7208e7ec5fc490c66
fxanhkhoa <fxanhkhoa@gmail.com>
2018-04-20 09:58:06 +0700

    add function dir_threshold

--- sobel_thresh.cpp
+++ sobel_thresh.cpp
@@ -27,29 +27,32 @@ int delta = 0;
 int scale = 1;
 
 Mat mag_thresh(Mat image, int sobel_kernel);
+Mat dir_threshold(Mat image, int sobel_kernel);
 
 int main(int argc, char** argv ){
   Mat frame, output;
   VideoCapture cap("videos/clip1_FPT.mp4");
   if (!cap.isOpened())
     return -1;
-  //while (1){
+  while (1){
     cap >> frame;
     resize(frame, frame, Size(800, 600));
     GaussianBlur(frame, frame, Size(3, 3), 0, 0, BORDER_DEFAULT);
     cvtColor(frame, frame, CV_BGR2GRAY, 1);
 
     output = Mat::zeros(frame.size(), CV_8U);
-    output = mag_thresh(frame, 3);
+    //output = mag_thresh(frame, 3);
+    //output = dir_threshold(frame, 3);
     //imshow("a", output);
     /*for (int i = 0; i < output.rows; i++)
       for (int j = 0; j < output.cols; j++){
         cout <<
       }*/
-    cout << output << endl;
-    //if (waitKey(30) == 27)
-      //break;
-  //}
+    //cout << output << endl;
+    //imshow("frame", frame);
+    if (waitKey(30) == 27)
+      break;
+  }
   return 0;
 }
 
@@ -63,10 +66,10 @@ Mat mag_thresh(Mat image, int sobel_kernel = 3){
   // Calculate the gradient magnitude
   float scale_factor = 0;
   Mat gradmag = Mat::zeros(image.size(), CV_8U);
-  unsigned char *input = (unsigned char*)(gradmag.data);
-  unsigned char *in_sobelx = (unsigned char*)(sobelx.data);
-  unsigned char *in_sobely = (unsigned char*)(sobely.data);
-  unsigned char *out = (unsigned char*)(binary_output.data);
+  //unsigned char *input = (unsigned char*)(gradmag.data);
+  //unsigned char *in_sobelx = (unsigned char*)(sobelx.data);
+  //unsigned char *in_sobely = (unsigned char*)(sobely.data);
+  //unsigned char *out = (unsigned char*)(binary_output.data);
 
   for (int i = 0; i < gradmag.rows - 1; i++)
     for (int j = 0; j < gradmag.cols - 1; j++){
@@ -101,17 +104,25 @@ Mat mag_thresh(Mat image, int sobel_kernel = 3){
     return binary_output;
 }
 
-void dir_threshold(Mat image, Mat binary_output, int sobel_kernel = 3){
+Mat dir_threshold(Mat image, int sobel_kernel = 3){
   // Calculate the x and y gradients
-  Mat sobelx, sobely, absgraddir;
+  Mat sobelx, sobely, binary_output;
   Sobel(image, sobelx, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
   Sobel(image, sobely, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
 
   // Take the absolute value of the gradient direction,
   // apply a threshold, and create a binary image result
-  for (int i = 0; i < sobelx.cols; i++)
-    for (int j = 0; j < sobelx.rows; j++){
-      absgraddir.at<int>(i,j) = atan(abs(sobely.at<int>(i,j)) / abs(sobelx.at<int>(i,j)));
-      //binary_output.at<double>
+  Mat absgraddir = Mat::zeros(image.size(), CV_8U);
+  binary_output = Mat::zeros(image.size(), CV_8U);
+  for (int i = 0; i < absgraddir.rows - 1; i++)
+    for (int j = 0; j < absgraddir.cols - 1; j++){
+      uchar x = sobelx.at<uchar>(i,j);
+      uchar y = sobely.at<uchar>(i,j);
+      double k = atan(double(abs(y)) / double(abs(x)));
+      //cout << k <<" ";
+      if ((k > min_thresh_angle) && (k < max_thresh_angle)){
+        binary_output.at<uchar>(i,j) = 1;
+      }
     }
+  return binary_output;
 }
